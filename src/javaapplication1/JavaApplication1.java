6433231238aaa597177944d9122c718f06321a4e
Vancha <Vancha@WINDOWS-T8H7VCJ>
2015-12-14 17:23:19 +0100

    partial solver code

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -11,11 +11,31 @@ package javaapplication1;
  */
 public class JavaApplication1 {
 
+    public int[][] board;
+    public int boardSize = 9;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public static void Solver(int[][] board)
+    {
+        
+        
+    }
+    public boolean isValidMove(int[][] board, int row, int column, int number)
+    {
+        
+            for(int i = 0; i < boardSize;i++)
+            {
+                if(board[row][i] == number) return false;
+                if(board[i][column] == number) return false;
+            }
+        
+        return true;
     }
     
 }
